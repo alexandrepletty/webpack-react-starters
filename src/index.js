e568c46889bb172112b0c4204495167cc52f1c73
Alexandre Pletty <alexandre.pletty@gmail.com>
2021-02-23 12:09:06 +0100

    build (webpack): add sass
    - setting up sass
    - removing babel config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 import React from 'react'
 import {render} from 'react-dom'
 
+// --- Style
+import './assets/css/stylesheet.scss'
+
 // --- Application
 import App from './App'
 
--- webpack/config/common.js
+++ webpack/config/common.js
@@ -1,5 +1,6 @@
 // --- Dependency
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 // --- Modules
 module.exports = {
@@ -7,12 +8,27 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.(s[ac]|c)ss$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', {
+					loader: 'postcss-loader',
+					options: {
+						postcssOptions: {
+							plugins: [
+								['postcss-preset-env', {
+									browsers: ['last 2 version', 'ie > 8']
+								}]
+							]
+						}
+					}
+				}, 'sass-loader']
+			},
+			{
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: ['@babel/preset-env','@babel/preset-react']
+						presets: ['@babel/preset-env', ['@babel/preset-react', {runtime: 'automatic'}]]
 					}
 				}
 			}
@@ -21,6 +37,7 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin(
 			{ template: './public/index.html' }
-		)
+		),
+		new MiniCssExtractPlugin()
 	]
 }
